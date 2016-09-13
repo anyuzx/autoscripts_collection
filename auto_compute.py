ebcbd75870cd3f8d3910a89bef1e1821f3f522ee
anyuzx <stefanshi1988@gmail.com>
2016-09-13 11:38:05 -0500

    update

---
+++ auto_compute.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import argparse
+
+
+def main(template, parameter, time, hpc):
+    work_path = os.environ['WORK']
+    if hpc == 'ls5':
+        sys.path.append('{}/myapps/Lammps_Template_Tool'.format(work_path))
+        myapps_path = $WORK/myapps
+    elif hpc == 'stampede':
+        sys.path.append('{}/stampede/myapps/Lammps_Template_Tool'.format(work_path))
+        myapps_path = $WORK/stampede/myapps
+    import ltt
+
+    if time is None:
+        sys.stdout.write('ERROR:Please specify a run time.\n')
+        sys.stdout.flush()
+        sys.exit(0)
+
+    parameter_file_lst = ltt.main(template, parameter)
+    njobs = len(parameter_file_lst)
+
+    with open('compute_complete.txt', 'w') as f:
+        for fp in parameter_file_lst:
+            f.write('python {}/H5MD_Analysis/AnalysisTool.py {} -l {}\n'.format(myapps_path, \
+                    fp, os.path.splitext(fp)[0]+'.log'))
+
+    with open('compute_complete.sh', 'w') as f:
+        f.write('#!/bin/bash\n')
+        f.write('#SBATCH -J cdp\n')
+        f.write('#SBATCH -o cdp.out\n')
+        f.write('#SBATCH -n {}\n'.format(njobs))
+        f.write('#SBATCH -p normal\n')
+        f.write('#SBATCH -t 00:{}:00\n'.format(time))
+        if hpc == 'ls5':
+            f.write('module load launcher\n')
+        elif hpc == 'stampede':
+            f.write('module load launcher/2.0\n')
+        if hpc == 'ls5':
+            f.write('export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins\n')
+        elif hpc == 'stampede':
+            f.write('export LAUNCHER_PLUGIN_DIR=$TACC_LAUNCHER_DIR/plugins\n')
+        f.write('export LAUNCHER_RMI=SLURM\n')
+        f.write('export LAUNCHER_JOB_FILE='+os.getcwd()+'/compute.txt\n')
+        f.write('$LAUNCHER_DIR/paramrun\n')
+
+if __name__  == '__main__':
+    parser = argparse.ArgumentParser(description='compute script.')
+    parser.add_argument('template', help='specify template file.')
+    parser.add_argument('parameter', help='specify parameter file.')
+    parser.add_argument('-time', help='specify time required in minute unit.', dest='time', type=int)
+    parser.add_argument('-hpc', help='specify HPC cluster. stampede or ls5.', dest='hpc')
+    args = parser.parse_args()
+
+    main(args.template, args.parameter, args.time, args.hpc)
--- auto_dump2h5md.py
+++ auto_dump2h5md.py
@@ -3,32 +3,57 @@ import glob
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='convert LAMMPS dump files to H5MD files.')
-parser.add_argument('-in', '--input', help='LAMMPS dump files path.', dest='input')
-parser.add_argument('-time', help='specify time required in minute unit.', dest='time', type=int)
-args = parser.parse_args()
+def main(input, time, hpc):
+    files = glob.glob(input)
+    nfiles = len(files)
 
-files = glob.glob(args.input)
-nfiles = len(files)
+    if hpc is None:
+        sys.stdout.write('Please specify HPC cluster. Options: stampede and ls5.')
+        sys.stdout.flush()
+        sys.exit(0)
 
-if args.time is None:
-    sys.stdout.write('ERROR:Please specify a run time.\n')
-    sys.stdout.flush()
-    sys.exit(0)
+    if hpc == 'ls5':
+        myapps_path = $WORK/myapps
+    elif hpc == 'stampede':
+        myapps_path = $WORK/stampede/myapps
 
-with open('dump2h5md.txt', 'w') as f:
-    for fp in files:
-        f.write('python $WORK/myapps/toolbox/dump2h5md.py {} {} -l {}\n'.format(fp, fp + '.h5', 'convert_' + fp + '.log'))
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(myapps_path+'/Lammps_Template_Tool')
+    import ltt
 
-with open('convert2h5md.sh', 'w') as f:
-    f.write('#!/bin/bash\n')
-    f.write('#SBATCH -J dump2h5md\n')
-    f.write('#SBATCH -o dump2h5md.out\n')
-    f.write('#SBATCH -n {}\n'.format(nfiles))
-    f.write('#SBATCH -p normal\n')
-    f.write('#SBATCH -t 00:{}:00\n'.format(args.time))
-    f.write('module load launcher\n')
-    f.write('export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins\n')
-    f.write('export LAUNCHER_RMI=SLURM\n')
-    f.write('export LAUNCHER_JOB_FILE={}/dump2h5md.txt\n'.format(os.getcwd()))
-    f.write('$LAUNCHER_DIR/paramrun\n')
+    if time is None:
+        sys.stdout.write('ERROR:Please specify a run time.\n')
+        sys.stdout.flush()
+        sys.exit(0)
+
+    with open('dump2h5md.txt', 'w') as f:
+        for fp in files:
+            f.write('python {}/toolbox/dump2h5md.py {} {} -l {}\n'.format(myapps_path, \
+                    fp, fp + '.h5', 'convert_' + fp + '.log'))
+
+    with open('ltt_parameter.temp', 'w') as f:
+        f.write('FILE: convert2h5md.sh\n')
+        f.write("job_name='dump2h5md'\n")
+        f.write("core_number={}\n".format(nfiles))
+        f.write("job_time={}\n".format(time))
+        if hpc == 'ls5':
+            f.write("job_file={}/dump2h5md.txt\n".format(os.getcwd()))
+        elif hpc == 'stampede':
+            f.write("job_file=dump2h5md.txt\n")
+
+    if hpc == 'ls5':
+        ltt.main('{}/launcher_template.ls5', '{}/ltt_parameter.temp'.format(script_path, script_path))
+    elif hpc == 'stampede':
+        ltt.main('{}/launcher_template.stampede', '{}/ltt_parameter.temp'.format(script_path, script_path))
+
+    #os.system("sbatch convert2h5md.sh")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='convert LAMMPS dump files to H5MD files.')
+    parser.add_argument('-in', '--input', help='LAMMPS dump files path.', dest='input')
+    parser.add_argument('-time', help='specify time required in minute unit.', dest='time', type=int)
+    parser.add_argument('-hpc', help='specify HPC cluster. stampede or ls5.', dest='hpc')
+    args = parser.parse_args()
+
+    main(args.input, args.time, args.hpc)
