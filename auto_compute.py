7930eb2638a2c4fffa1edc0e6834afa486a88205
anyuzx <stefanshi1988@gmail.com>
2016-09-18 11:39:11 -0500

    update

--- auto_compute.py
+++ auto_compute.py
@@ -26,24 +26,25 @@ def main(template, parameter, time, hpc):
             f.write('python {}/H5MD_Analysis/AnalysisTool.py {} -l {}\n'.format(myapps_path, \
                     fp, os.path.splitext(fp)[0]+'.log'))
 
-    with open('compute.sh', 'w') as f:
-        f.write('#!/bin/bash\n')
-        f.write('#SBATCH -J cdp\n')
-        f.write('#SBATCH -o cdp.out\n')
-        f.write('#SBATCH -n {}\n'.format(njobs))
-        f.write('#SBATCH -p normal\n')
-        f.write('#SBATCH -t 00:{}:00\n'.format(time))
+    with open('ltt_parameter.temp', 'w') as f:
+        f.write('FILENAME=compute.sh\n')
+        f.write("job_name=['compute']\n")
+        f.write("core_number=[{}]\n".format(nfiles))
+        f.write("job_time=[{}]\n".format(time))
         if hpc == 'ls5':
-            f.write('module load launcher\n')
+            f.write("job_file=['{}/compute.txt']\n".format(os.getcwd()))
         elif hpc == 'stampede':
-            f.write('module load launcher/2.0\n')
-        if hpc == 'ls5':
-            f.write('export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins\n')
-        elif hpc == 'stampede':
-            f.write('export LAUNCHER_PLUGIN_DIR=$TACC_LAUNCHER_DIR/plugins\n')
-        f.write('export LAUNCHER_RMI=SLURM\n')
-        f.write('export LAUNCHER_JOB_FILE='+os.getcwd()+'/compute.txt\n')
-        f.write('$LAUNCHER_DIR/paramrun\n')
+            f.write("job_file=['compute.txt']\n")
+
+    if hpc == 'ls5':
+        ltt.ltt('{}/launcher_template.ls5'.format(script_path), 'ltt_parameter.temp')
+    elif hpc == 'stampede':
+        ltt.ltt('{}/launcher_template.stampede'.format(script_path), 'ltt_parameter.temp')
+
+    os.remove('ltt_parameter.temp')
+
+    if run:
+        os.system("sbatch convert2h5md.sh")
 
 if __name__  == '__main__':
     parser = argparse.ArgumentParser(description='compute script.')
@@ -51,6 +52,7 @@ if __name__  == '__main__':
     parser.add_argument('parameter', help='specify parameter file.')
     parser.add_argument('-time', help='specify time required in minute unit.', dest='time', type=int)
     parser.add_argument('-hpc', help='specify HPC cluster. stampede or ls5.', dest='hpc')
+    parser.add_argument('-run', help='Submit the job directly. Otherwise this script just generate a slurm script file', action='store_true', dest='run')
     args = parser.parse_args()
 
-    main(args.template, args.parameter, args.time, args.hpc)
+    main(args.template, args.parameter, args.time, args.hpc, args.run)
